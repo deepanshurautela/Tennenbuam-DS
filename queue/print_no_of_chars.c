03b0c90ed28edc5eafac393ad11771874515efcb
lib2 <admin@lib2s-iMac.local>
2018-07-13 14:18:58 +0530

    Was adding \n to the end and not \0

--- queue/print_no_of_chars.c
+++ queue/print_no_of_chars.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAXLEN 100
+
+/*
+STACK OVERFLOW SOL FOR SPLITTING THE STRING INTO TWO
+
+#define MAXLEN 80
+
+int
+main(int argc, char *argv[])
+{
+    char buf[MAXLEN];
+    printf("what is the string?: ");
+    fgets (buf, MAXLEN, stdin);
+    for(int i = 0; i<strlen(buff); i++)
+      printf("buff[%d]: %c\n", i, buf[i]);
+    return(0);
+}
+
+*/
+
 
 typedef struct node{
 	int count; 
@@ -17,22 +37,22 @@ Node *getNode(char c){
 }
 
 void char_list(Node **head, char c){
-	printf("Char list was called\n");
+	//printf("Char list was called\n");
 	Node *newNode = getNode(c);
 	Node *temp = (*head);
 	if (temp == NULL){
 			(*head) = newNode;
-			printf("Just set null node to new node containing %c\n", newNode -> a);
+			//printf("Just set null node to new node containing %c\n", newNode -> a);
 	}
 	else{
-		while(temp -> next != NULL && temp -> a != c)
+		while(temp -> next != NULL && temp -> a != c && c != '\n') //3rd condition so doesn't make node for null char
 			temp = temp -> next;
 				if (temp -> next == NULL && temp ->a != c){ //Added the second condition otherwise will create nodes for the same alphabet. 
 					temp -> next = newNode;
-					printf("Reached the end couldn't find anything. Creating a new node for %c \n", newNode ->a);
+					//printf("Reached the end couldn't find anything. Creating a new node for %c \n", newNode ->a);
 					return;
 				}
-				printf("Current count of %c is %d\n",temp -> a ,temp -> count);
+				//printf("Current count of %c is %d\n",temp -> a ,temp -> count);
 				temp -> count++;
 				free(newNode);
 		
@@ -57,12 +77,12 @@ void print_all(Node *head){
 int main(int argc, char const *argv[])
 {
 	Node *head = NULL;
-	char arr[] = {'c','a','a','a','c','c','c','p','g','t','w','g','g','g','g'};
-	int size = (sizeof(arr)/sizeof(arr[0]));
-	for (int i = 0; i < size; ++i)
-	{
-		char_list(&head, arr[i]);
-	}
+	//char arr[] = {'c','a','a','a','c','c','c','p','g','t','w','g','g','g','g'};
+    char buf[MAXLEN];
+    printf("what is the string?:\n");
+    fgets (buf, MAXLEN, stdin);
+    for(int i = 0; i<strlen(buf); i++)
+		char_list(&head, buf[i]);
 	print_all(head);
 	return 0;
 }
