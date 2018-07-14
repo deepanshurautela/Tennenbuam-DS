9c1aab77de3153b14ac5bd3ebba060e2c29bc12d
Deepanshu Rautela <deepanshu@Deepanshus-MacBook-Air.local>
2018-07-14 14:23:00 +0530

    Initial Commit

---
+++ queue/circular_queue_linked_list.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define MAXSIZE 1024 * 1024
+
+typedef struct node{
+	char *name;
+	struct node *next;
+}Node;
+
+Node *getNode(char *name){
+	Node *newNode = (Node *)malloc(sizeof(Node));
+	newNode -> name = name;
+	newNode -> next = NULL;
+	return newNode;
+}
+
+void enQueue(Node **last, char *name){
+	Node *newNodeTemp = getNode(name);
+	Node *temp = (*last);
+
+	if ((*last) == NULL)
+	{
+		newNodeTemp -> next = newNodeTemp;
+		(*last) = newNodeTemp;
+	}
+
+	else{
+		newNodeTemp -> next = temp -> next;
+		temp -> next = newNodeTemp;
+		(*last) = newNodeTemp;
+	}
+}
+
+void printall(Node *last){
+	Node *temp = last;
+		while(temp -> next != temp)
+			printf("%s\n",temp -> name);
+}
+
+int main(int argc, char const *argv[])
+{	char buf[MAXSIZE];
+	Node *head = NULL;
+	for (int i = 0; i < 2; ++i)
+	{
+		printf("Enter a name:- \n");
+		fgets(buf, MAXSIZE, stdin);
+		enQueue(&head, buf);
+	}
+	printall(head);
+	return 0;
+}
