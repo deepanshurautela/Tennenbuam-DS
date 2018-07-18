970455cd35c2254de80048693c9753218dd7b5aa
lib2 <admin@lib2s-iMac.local>
2018-07-18 15:37:25 +0530

    Initial Commit

---
+++ queue/josephus.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAXSIZE 1000
+
+//Nice Line - StackOverFlow
+//typedef struct string { char x[MAXSIZE]; } string;
+
+typedef struct node{
+	struct node *next;
+	char *name;
+}Node;
+
+Node* getNode(char *name){
+	Node *newNode = (Node *)malloc(sizeof(Node));
+	newNode -> next = NULL;
+	newNode -> name = name;
+	return newNode;
+}
+
+
+void enQueue(Node **head, char* name){
+	Node *temp = (*head);
+	Node *newNode = getNode(name);
+		if ((*head) == NULL)
+			(*head) = newNode;
+		else{
+			while(temp -> next != NULL)
+				temp = temp -> next;
+			temp -> next = newNode;
+		}
+}
+
+void pop(Node **head, Node **del_node){
+	Node *temp = (*head);
+		if (*head == NULL)
+			return;
+		while(temp -> next != (*del_node))
+				temp = temp -> next;
+			temp -> next = temp -> next -> next;
+}
+
+void print_all(Node *head){
+	Node* temp = head;
+		while(temp != NULL){
+			printf("%s\n",temp -> name);
+			temp = temp -> next;
+		}
+}
+
+
+void josephus(Node** head, int size){
+	time_t t;
+	srand((unsigned) time(&t));
+	int random = rand() % size + 1;
+	printf("Random number is %d\n",random);
+	Node *temp = (*head);
+		for (int i = 0; i < random; ++i)
+		{
+			temp = temp -> next;
+		}
+			temp = temp -> next;
+			pop(head, &temp);
+}
+
+int main(int argc, char const *argv[])
+{
+	Node *head = NULL;
+	enQueue(&head, "One");
+	enQueue(&head, "Two");
+	enQueue(&head, "Three");
+	enQueue(&head, "Four");
+	print_all(head);
+	printf("\n\n\n");
+	josephus(&head, 4);
+	return 0;	
+}
